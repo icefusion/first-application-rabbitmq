debc3bca9d0358ecf7cc81eb39c72238dd2141fc
willrocker <icefusion@icefusion.com.br>
2018-03-16 08:51:01 -0300

    Criado Send e Receive utilizando php com a lib do rabbitmq

--- src/send.php
+++ src/send.php
@@ -1,6 +1,13 @@
 <?php
 
-require_once __DIR__ . '../vendor/autoload.php';
+/**
+ * Created by PhpStorm.
+ * User: willrocker
+ * Date: 16/03/18
+ * Time: 08:42
+ */
+
+require_once __DIR__ . '/../vendor/autoload.php';
 
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Message\AMQPMessage;
@@ -9,9 +16,13 @@ use PhpAmqpLib\Message\AMQPMessage;
 $connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
 $channel = $connection->channel();
 
-$channel->queue_declare('hello', true, true, false, false);
+$channel->queue_declare('hello', false, true, false, false);
 
 $msg = new AMQPMessage('Hello World!');
 $channel->basic_publish($msg, '', 'hello');
 
 echo " [x] Sent 'Hello World!'\n";
+
+//$channel->close();
+//
+//$connection->close();
